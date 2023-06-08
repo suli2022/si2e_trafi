7312c27c155bfc59d0fd023e6a053f85dbcd92a2
Nagy Janos <nagy@zold.lan>
2023-06-08 19:32:23 +0200

    Kész

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View
+} from 'react-native';
+import Button from './components/Button';
+
 
 export default function App() {
+  const [count, setCounter] = useState(0);
+  function increment() {
+    let res = count+1;
+    setCounter(res);
+  }
+  function decrement() {
+    let res = count-1;
+    setCounter(res);
+  }
+  function reset() {
+    setCounter(0);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Forgalom</Text>
+      <Text style={styles.title}>számláló</Text>
+
+      <Text style={styles.count}>{count}</Text>
+      
+      <Button title="Növel" 
+      onPress={increment} />
+
+      <Button title="Csökkent" 
+      onPress={decrement} />
+
+      <Button title="Töröl" 
+      onPress={reset} />
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'navy',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 32,
+    color: 'white',
+  },
+  count: {
+    fontSize: 54,
+    color: 'yellow',
   },
 });
